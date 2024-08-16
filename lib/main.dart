65818f5c02cf65c8331dfa004a861cd3a6749ab0
mudo <dgny362@gmail.com>
2024-08-16 23:00:00 +0300

    giriş

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tabu/myWidgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,5 +9,39 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                isimAlma(
+                  ad: 'Takım 1',
+                  hint: 'TAKIM 1 İsmini girin',
+                  focus: true,
+                ),
+                isimAlma(
+                  ad: 'Takım 2',
+                  hint: 'TAKIM 2 İsmini girin',
+                  focus: false,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'BAŞLA',
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.indigoAccent)),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
